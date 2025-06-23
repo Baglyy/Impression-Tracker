9a69f10130a2cffb68451cc0631f6a915d2ea873
Boris GIARD <boris.giard@mediakeys.com>
2025-06-23 13:18:43 +0200

    review

--- server/main.go
+++ server/main.go
@@ -1,17 +1,16 @@
 package main
 
 import (
+	"context"
 	"errors"
-	"context" 
-	"log"    
-	"net"     
+	"log"
+	"net"
 
-	// Import du code que buf a généré
+	"github.com/redis/go-redis/v9"      // Lib pour Dragonfly/Redis
+	"google.golang.org/grpc"            // lib grpc
+	"google.golang.org/grpc/reflection" // lib test grpcurl
+
 	pb "github.com/Baglyy/impression_tracking/proto" // Code généré à partir de proto
-	"github.com/redis/go-redis/v9" // Lib pour Dragonfly/Redis
-	"google.golang.org/grpc" // lib grpc
-	"google.golang.org/grpc/reflection" // lib test grpcurl 
-
 )
 
 // Référence au client Dragonfly.
@@ -20,6 +19,7 @@ type server struct {
 	dragonflyClient *redis.Client
 }
 
+// NOTE: Not used
 // Créer une nouvelle instance du serveur
 func NewServer(dflyClient *redis.Client) *server {
 	return &server{dragonflyClient: dflyClient}
@@ -27,38 +27,40 @@ func NewServer(dflyClient *redis.Client) *server {
 
 // Implémentation de la méthode RPC.
 func (s *server) TrackImpression(ctx context.Context, req *pb.TrackImpressionRequest) (*pb.TrackImpressionResponse, error) {
-	
 	// 1. Récupérer l'ID de la publicité depuis la requête.
 	adID := req.GetAdId()
 
-    // 2. Valider l'input (Point Bonus).
+	// 2. Valider l'input (Point Bonus).
 	if adID == "" {
+		// NOTE: Error in French ?
 		return nil, errors.New("L'ad_id est vide")
 	}
 
-    // 3. Construire la clé à utiliser dans Dragonfly.
+	// 3. Construire la clé à utiliser dans Dragonfly.
 	key := adID
 
-    // 4. Utiliser la commande atomique INCR de Redis.
-	newCount, err := s.dragonflyClient.Incr(ctx, key).Result() 	// s.dragonflyClient donne accès au champ qu'on a définie dans struct.
-
+	// NOTE: Can use adID as key, no need to reassign a var
+	// 4. Utiliser la commande atomique INCR de Redis.
+	newCount, err := s.dragonflyClient.Incr(ctx, key).Result() // s.dragonflyClient donne accès au champ qu'on a définie dans struct.
 	// Test d'erreur
 	if err != nil {
 		log.Println("Echec de l'incrémentation de ad_id : ", adID, err)
 		return nil, err
 	}
 
-    // 5. Si tout s'est bien passé, construire la réponse.
+	// NOTE: Can return directly the reponse
+	// 5. Si tout s'est bien passé, construire la réponse.
 	reponse := &pb.TrackImpressionResponse{
-		AdId: adID,
+		AdId:             adID,
 		TotalImpressions: newCount,
 	}
 
 	return reponse, nil
 }
 
+// NOTE: Don't do logs in french
 func main() {
-
+	// NOTE: This should be near the Serve below. Group things that work together.
 	listener, err := net.Listen("tcp", ":50051")
 	if err != nil {
 		log.Fatalln("Echec de l'ouverture du port 50051 : ", err)
@@ -71,6 +73,7 @@ func main() {
 	// Création et enregistrement du serveur gRPC
 	grpcServer := grpc.NewServer()
 
+	// NOTE: You should use your NewServer function to initialize the server.
 	// Passer au serveur la connexion à la BDD.
 	myServer := &server{
 		dragonflyClient: dbClient,
--- server/main_test.go
+++ server/main_test.go
@@ -5,6 +5,7 @@ import (
 	"testing"
 
 	"github.com/go-redis/redismock/v9"
+
 	pb "github.com/Baglyy/impression_tracking/proto"
 )
 
@@ -13,8 +14,9 @@ func TestTrackImpression(t *testing.T) {
 
 	// Sous-test 1: Le cas où tout se passe bien
 	t.Run("Cas de succès", func(t *testing.T) {
-
 		// Créer une fausse base de données pour ce test
+
+		// NOTE: Nice usage of mock
 		db, mock := redismock.NewClientMock()
 
 		// Créer le serveur avec la fausse base de données
@@ -22,17 +24,18 @@ func TestTrackImpression(t *testing.T) {
 
 		adID := "ad_test_123"
 
+		// NOTE: it's not a key but a value.
 		key := int64(5)
 
 		// Appel à Incr avec adID. Si l'appel est réalisé, réponse = 5
 		mock.ExpectIncr(adID).SetVal(key)
 
+		// NOTE: Doesn't simulate but create a message then send it to the server
 		// Simuler une requête gRPC
 		req := &pb.TrackImpressionRequest{AdId: adID}
 
 		// Appel de TrackImpression
 		res, err := server.TrackImpression(ctx, req)
-
 		// Vérifier les erreurs
 		if err != nil {
 			t.Fatal("Echec du test")
@@ -42,11 +45,9 @@ func TestTrackImpression(t *testing.T) {
 		if res.GetTotalImpressions() != key {
 			t.Fatal("Echec du test")
 		}
-
 	})
 
 	t.Run("Cas avec ad_id vide", func(t *testing.T) {
-
 		db, _ := redismock.NewClientMock()
 		s := &server{dragonflyClient: db}
 
@@ -56,10 +57,11 @@ func TestTrackImpression(t *testing.T) {
 		// On appelle notre fonction à tester. On ignore la réponse (`_`)
 		_, err := s.TrackImpression(ctx, req)
 
+		// NOTE: Usage of t.Fatal instead of require/assert
 		// Test erreur
 		if err == nil {
 			t.Fatal("Echec du test")
-		} 
-
+		}
 	})
 }
+
